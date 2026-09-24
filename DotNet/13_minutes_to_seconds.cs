eed1e5891f6e8c82022fafb7a9f6904c6d89ecd0
Mirza <137265365+HM-25@users.noreply.github.com>
2026-09-24 13:08:50 +0200

    DotNet: rename exercises to descriptive names, fix two small bugs, add README

---
+++ DotNet/13_minutes_to_seconds.cs
@@ -0,0 +1,18 @@
+// Exercise 13: Convert minutes to seconds
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int minutes;
+
+        Console.Write("Unesi broj minuta: ");
+        minutes = int.Parse(Console.ReadLine());
+
+        int seconds = minutes * 60;
+
+        Console.WriteLine("{0} minuta je {1} sekundi", minutes, seconds);
+        Console.ReadLine();
+    }
+}
